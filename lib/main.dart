577b732f0fa5c1ac721e46fa10e212026f0a08ea
natumn <natsume.pcuse75@gmail.com>
2018-10-21 01:01:37 +0900

    document UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,130 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(SurvivalBook());
+}
 
-class MyApp extends StatelessWidget {
+class SurvivalBook extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Survival Book',
-      theme: new ThemeData(
-        primaryColor: Colors.blue[700],
-      ),
-      home: SurvivalBook(),
-    );
-  }
-}
-
-class SurvivalBook extends StatefulWidget {
-  @override
-  SurvivalBookState createState() => new SurvivalBookState();
-}
-
-class SurvivalBookState extends State<SurvivalBook> {
-  final List<WordPair> _suggestions = <WordPair>[];
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: AppBar(
-        title: Text('Survival Book'),
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(5));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      onTap: () {
-        setState(() {
-          Navigator.of(context).push(
-            new MaterialPageRoute<void>(
-              builder: (BuildContext context) {
-                return new Scaffold(
-                  body: new Genre(),
-                );
-              },
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(text: "ducument", icon: Icon(Icons.insert_drive_file)),
+                Tab(text: "news", icon: Icon(Icons.event_note)),
+                Tab(text: "map", icon: Icon(Icons.map)),
+              ],
             ),
-          );
-        });
-      },
-    );
-  }
-}
-
-class Genre extends StatefulWidget {
-  @override
-  GenreState createState() => new GenreState();
-}
-
-class GenreState extends State<Genre> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('応急処置'),
-        actions: <Widget>[
-          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
-        ],
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      new MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final List<Widget> divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-
-          return new Scaffold(
-            appBar: new AppBar(
-              title: const Text('Saved Suggestions'),
-            ),
-            body: new ListView(children: divided),
-          );
-        },
+            title: Text('Survival Book'),
+          ),
+          body: TabBarView(
+            children: [
+              Genres(),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          ),
+        ),
       ),
     );
   }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(5));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
 }
 
-class Flows extends StatefulWidget {
+class Genres extends StatefulWidget {
   @override
-  FlowsState createState() => new FlowsState();
+  GenresState createState() => new GenresState();
 }
 
-class FlowsState extends State<Flows> {
+class GenresState extends State<Genres> {
   @override
   final List<WordPair> _suggestions = <WordPair>[];
   final Set<WordPair> _saved = new Set<WordPair>(); // Add this line.
@@ -132,43 +48,7 @@ class FlowsState extends State<Flows> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('応急処置'),
-        actions: <Widget>[
-          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
-        ],
-      ),
       body: _buildSuggestions(),
-    );
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      new MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final Iterable<ListTile> tiles = _saved.map(
-            (WordPair pair) {
-              return new ListTile(
-                title: new Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final List<Widget> divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-
-          return new Scaffold(
-            appBar: new AppBar(
-              title: const Text('Saved Suggestions'),
-            ),
-            body: new ListView(children: divided),
-          );
-        },
-      ),
     );
   }
 
@@ -178,66 +58,114 @@ class FlowsState extends State<Flows> {
         itemBuilder: (context, i) {
           if (i.isOdd) return Divider();
           final index = i ~/ 2;
+
           if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(5));
+            _suggestions.addAll(generateWordPairs().take(10));
           }
           return _buildRow(_suggestions[index]);
         });
   }
 
   Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair); // Add this line.
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
-        pair.asPascalCase,
+        "応急処置",
         style: _biggerFont,
       ),
       trailing: new Icon(
-        alreadySaved ? Icons.check_circle : Icons.check_circle_outline,
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.red : null,
       ),
       onTap: () {
-        setState(() {
-          /*
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-          */
-          Navigator.of(context).push(
-            new MaterialPageRoute<void>(
-              builder: (BuildContext context) {
-                /*
-                final Iterable<ListTile> tiles = _saved.map(
-                  (WordPair pair) {
-                    return new ListTile(
-                      title: new Text(
-                        pair.asPascalCase,
-                        style: _biggerFont,
-                      ),
-                    );
-                  },
-                );
-                final List<Widget> divided = ListTile.divideTiles(
-                  context: context,
-                  tiles: tiles,
-                ).toList();
-                */
-                final content = <Widget>[
-                  new Text("止血する方法だよ。\n血を止めるよ。"),
-                ];
+        Navigator.of(context).push(
+          new MaterialPageRoute(
+            builder: (BuildContext context) {
+              final Iterable<ListTile> tiles = _saved.map(
+                (WordPair pair) {
+                  return new ListTile(
+                    title: new Text(
+                      pair.asPascalCase,
+                      style: _biggerFont,
+                    ),
+                  );
+                },
+              );
+              final List<Widget> divided = ListTile.divideTiles(
+                context: context,
+                tiles: tiles,
+              ).toList();
 
-                return new Scaffold(
-                  appBar: new AppBar(
-                    title: const Text('止血'),
-                  ),
-                  body: new ListView(children: content),
-                );
-              },
-            ),
-          );
+              return new Scaffold(
+                appBar: new AppBar(
+                  title: const Text('応急処置'),
+                ),
+                body: Actions(),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+}
+
+//
+class Actions extends StatefulWidget {
+  @override
+  ActionsState createState() => new ActionsState();
+}
+
+class ActionsState extends State<Actions> {
+  @override
+  final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = new Set<WordPair>();
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+          final index = i ~/ 2;
+
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
         });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        "止血",
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        Navigator.of(context).push(
+          new MaterialPageRoute(
+            builder: (BuildContext context) {
+              return new Scaffold(
+                appBar: new AppBar(
+                  title: const Text('止血'),
+                ),
+                body: Text("\n止血の方法が乗っているよ！"),
+              );
+            },
+          ),
+        );
       },
     );
   }
