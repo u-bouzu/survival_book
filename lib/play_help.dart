59b5e82569e53ab4c7e406c511c4d99b2e616510
dennougorilla <dennougorilla@gmail.com>
2018-10-21 14:42:50 +0900

    add play help

---
+++ lib/play_help.dart
@@ -0,0 +1,59 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+import 'player_widget.dart';
+
+void main() {
+  runApp(new MaterialApp(home: new ExampleApp()));
+}
+
+class ExampleApp extends StatefulWidget {
+  @override
+  _ExampleAppState createState() => new _ExampleAppState();
+}
+
+class _ExampleAppState extends State<ExampleApp> {
+  AudioCache audioCache = new AudioCache();
+  AudioPlayer advancedPlayer = new AudioPlayer();
+
+  Widget _tab(List<Widget> children) {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: children
+              .map((w) => Container(child: w, padding: EdgeInsets.all(6.0)))
+              .toList(),
+        ),
+      ),
+    );
+  }
+
+  Widget _btn(String txt, VoidCallback onPressed) {
+    return ButtonTheme(
+        minWidth: 48.0,
+        child: RaisedButton(child: Text(txt), onPressed: onPressed));
+  }
+
+  Widget localAsset() {
+    return _tab([
+      Text('Play Help(English)'),
+      _btn('Play', () => audioCache.play('helpEn.mp3')),
+      Text('Play Help(Japanese)'),
+      _btn('Play', () => audioCache.play('helpJa.mp3')),
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        body: TabBarView(
+          children: [localAsset()],
+        ),
+      ),
+    );
+  }
+}
