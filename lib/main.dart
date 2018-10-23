693408edba0988aadca67ce07a46ea4869f738d4
natumn <natsume.pcuse75@gmail.com>
2018-10-23 23:25:34 +0900

    fix-doc-view

--- lib/main.dart
+++ lib/main.dart
@@ -186,7 +186,8 @@ class GenresState extends State<Genres> {
                           appBar: new AppBar(
                             title: const Text('地震対策'),
                           ),
-                          body: Text("\n1. 【発災直後の安全確保】\n発災直後は、自分の身の安全の確保が必要です。落下物に気を付けつつ、大きな什器等から離れて机の下等に隠れて様子を見守りましょう。\n\n2. 【津波からの避難】\n津波の危険性がある場合には、早急に高台等の指定避難場所に避難することが必要です。特に津波の危険性が指摘されている地域では、大きな揺れを感じたら素早く避難を開始することが求められます。津波は第２波や第３波が最大波高となる場合が多く、一旦津波が引いた場合でも沿岸部や浸水地域には近づかないようにしましょう。\n\n 3.  【２つの安全確認】\n　安全な場所に避難するかどうかは、建物の被災状況と共に、土砂災害や堤防決壊等による影響も踏まえて判断するようにしましょう。"),
+                          body: Text(
+                              "\n1. 【発災直後の安全確保】\n発災直後は、自分の身の安全の確保が必要です。落下物に気を付けつつ、大きな什器等から離れて机の下等に隠れて様子を見守りましょう。\n\n2. 【津波からの避難】\n津波の危険性がある場合には、早急に高台等の指定避難場所に避難することが必要です。特に津波の危険性が指摘されている地域では、大きな揺れを感じたら素早く避難を開始することが求められます。津波は第２波や第３波が最大波高となる場合が多く、一旦津波が引いた場合でも沿岸部や浸水地域には近づかないようにしましょう。\n\n 3.  【２つの安全確認】\n　安全な場所に避難するかどうかは、建物の被災状況と共に、土砂災害や堤防決壊等による影響も踏まえて判断するようにしましょう。"),
                         );
                       },
                     ),
@@ -198,11 +199,10 @@ class GenresState extends State<Genres> {
         });
   }
 
-  @override
-  final List<WordPair> _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = new Set<WordPair>(); // Add this line.
+  final _suggestions = ["ファーストエイド", "料理", "天気の予測"];
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _buildSuggestions(),
@@ -212,50 +212,26 @@ class GenresState extends State<Genres> {
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
+        itemCount: _suggestions.length,
         itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
-          final index = i ~/ 2;
-
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
+          // if (i.isOdd) return Divider();
+          return _buildRow(_suggestions[i]);
         });
   }
 
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
+  Widget _buildRow(String gener) {
     return ListTile(
       title: Text(
-        "ファーストエイド",
+        gener,
         style: _biggerFont,
-      ),
-      trailing: new Icon(
-        alreadySaved ? Icons.check_circle : Icons.check_circle_outline,
-        color: alreadySaved ? Colors.red : null,
       ),
       onTap: () {
         Navigator.of(context).push(
           new MaterialPageRoute(
             builder: (BuildContext context) {
-              final Iterable<ListTile> tiles = _saved.map(
-                (WordPair pair) {
-                  return new ListTile(
-                    title: new Text(
-                      pair.asPascalCase,
-                      style: _biggerFont,
-                    ),
-                  );
-                },
-              );
-              final List<Widget> divided = ListTile.divideTiles(
-                context: context,
-                tiles: tiles,
-              ).toList();
-
               return new Scaffold(
                 appBar: new AppBar(
-                  title: const Text('ファーストエイド'),
+                  title: Text(gener),
                 ),
                 body: Actions(),
               );
@@ -275,9 +251,16 @@ class Actions extends StatefulWidget {
 
 class ActionsState extends State<Actions> {
   @override
-  final Set<WordPair> _saved = new Set<WordPair>();
+  final _suggestions = [
+    "外傷の手当",
+    "腕の怪我の手当",
+    "休ませる姿勢",
+    "捻挫の手当",
+    "頭部の怪我の手当",
+    "直接圧迫止血法"
+  ];
+  final _saved = new Set<String>();
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
-  String title = "";
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -288,38 +271,30 @@ class ActionsState extends State<Actions> {
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
+        itemCount: _suggestions.length,
         itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
-          final index = i ~/ 2;
-
-          /*
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          */
-          return _buildRow();
+          // if (i.isOdd) return Divider();
+          return _buildRow(_suggestions[i]);
         });
   }
 
-  Widget _buildRow() {
-    // final bool alreadySaved = _saved.contains(pair);
+  Widget _buildRow(String title) {
+    final bool alreadySaved = _saved.contains(title);
     return ListTile(
       title: Text(
-        "外傷の手当",
+        title,
         style: _biggerFont,
       ),
-      /*
       trailing: new Icon(
         alreadySaved ? Icons.check_circle : Icons.check_circle_outline,
         color: alreadySaved ? Colors.red : null,
       ),
-      */
       onTap: () {
         String title;
         String content;
+        /*
         String gener;
         String img;
-        /*
         Directory appDocDir =  getApplicationDocumentsDirectory();
         String appDocPath = appDocDir.path;
         */
@@ -332,11 +307,9 @@ class ActionsState extends State<Actions> {
         // Map<String, dynamic> document = json.decode(fileContents.toString());
         // debugPrint(Directory.current.toString());
         title = document['title'];
-        debugPrint(title);
         content = document['content'];
-        debugPrint(content);
-        gener = document['gener'];
-        img = document['img'];
+        // gener = document['gener'];
+        // img = document['img'];
         // return document;
         // });
 
